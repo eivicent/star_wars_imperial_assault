d7bdd4f5d4ccffbc16fb34f2679bfd87727504ab
Vicent Boned <vicent.boned.11@gmail.com>
2020-01-17 16:25:43 +0100

    Updating scrapping of list of items

--- dice.R
+++ dice.R
@@ -1,12 +1,12 @@
 dice <- function(type, amount, detail = F){
   
   aux <- switch(type, 
-   	red		= matrix(c(c(1,0,0),c(2,0,0),c(2,0,0),c(2,1,0),c(3,0,0),c(3,0,0)), byrow = T, ncol = 3),
-  	blue	= matrix(c(c(0,1,2),c(1,0,2),c(2,0,3),c(1,1,3),c(2,0,4),c(1,0,5)), byrow = T, ncol = 3),
-  	green	= matrix(c(c(0,1,1),c(1,1,1),c(2,0,1),c(1,1,2),c(2,0,2),c(2,0,3)), byrow = T, ncol = 3),
-  	yellow	= matrix(c(c(0,1,0),c(1,2,0),c(2,0,1),c(1,1,1),c(0,1,2),c(1,0,2)), byrow = T, ncol = 3)
-  	)
-
+                red		= matrix(c(c(1,0,0),c(2,0,0),c(2,0,0),c(2,1,0),c(3,0,0),c(3,0,0)), byrow = T, ncol = 3),
+                blue	= matrix(c(c(0,1,2),c(1,0,2),c(2,0,3),c(1,1,3),c(2,0,4),c(1,0,5)), byrow = T, ncol = 3),
+                green	= matrix(c(c(0,1,1),c(1,1,1),c(2,0,1),c(1,1,2),c(2,0,2),c(2,0,3)), byrow = T, ncol = 3),
+                yellow	= matrix(c(c(0,1,0),c(1,2,0),c(2,0,1),c(1,1,1),c(0,1,2),c(1,0,2)), byrow = T, ncol = 3)
+  )
+  
   aux2 <- sample.int(n = nrow(aux), size = amount, replace = T)
   
   if(detail){
@@ -20,21 +20,21 @@ dice <- function(type, amount, detail = F){
     output <- list(summary = summary)
   }
   
-
+  
   return(output)
-
+  
 }
 pair_of_dices <- function(dice1, dice2, amount_of_pairs){
-
-	aux1 <- dice(dice1, amount_of_pairs, detail = T)
-	aux2 <- dice(dice2, amount_of_pairs, detail = T)
-
-	results <- aux1$results + aux2$results
-	summary <- colMeans(results)
-
-	output <- list(results = results, summary = summary)
-	return(output)
-
+  
+  aux1 <- dice(dice1, amount_of_pairs, detail = T)
+  aux2 <- dice(dice2, amount_of_pairs, detail = T)
+  
+  results <- aux1$results + aux2$results
+  summary <- colMeans(results)
+  
+  output <- list(results = results, summary = summary)
+  return(output)
+  
 }
 out <- pair_of_dices("red", "blue", 10000)
 
@@ -42,4 +42,10 @@ library(rvest)
 
 items_web <- read_html("https://imperial-assault.fandom.com/wiki/Item_Card_-_By_Tier/Product")
 
-list_of_items <- items_web %>% html_nodes("")
+items_web %>% html_attr()
+
+ups <- items_web %>% 
+  html_node(xpath = "//*[(@id = 'WikiaArticle')]/*[(@id = 'mw-content-text')]/ul") 
+
+ups2 <- ups %>% 
+  html_nodes(xpath = ".//a") %>% html_attrs()
