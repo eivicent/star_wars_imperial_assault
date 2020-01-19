e0e42230917d2737eedba83a0fa9f7b14f8eb5b7
Vicent Boned - Home <vicent.boned.11@gmail.com>
2020-01-20 00:31:55 +0100

    Conversion to function and apply

--- dice.R
+++ dice.R
@@ -1,10 +1,13 @@
+rm(list =ls())
+library(rvest)
+
 dice <- function(type, amount, detail = F){
   
   aux <- switch(type, 
-                red		= matrix(c(c(1,0,0),c(2,0,0),c(2,0,0),c(2,1,0),c(3,0,0),c(3,0,0)), byrow = T, ncol = 3),
-                blue	= matrix(c(c(0,1,2),c(1,0,2),c(2,0,3),c(1,1,3),c(2,0,4),c(1,0,5)), byrow = T, ncol = 3),
-                green	= matrix(c(c(0,1,1),c(1,1,1),c(2,0,1),c(1,1,2),c(2,0,2),c(2,0,3)), byrow = T, ncol = 3),
-                yellow	= matrix(c(c(0,1,0),c(1,2,0),c(2,0,1),c(1,1,1),c(0,1,2),c(1,0,2)), byrow = T, ncol = 3)
+                Red		= matrix(c(c(1,0,0),c(2,0,0),c(2,0,0),c(2,1,0),c(3,0,0),c(3,0,0)), byrow = T, ncol = 3),
+                Blue	= matrix(c(c(0,1,2),c(1,0,2),c(2,0,3),c(1,1,3),c(2,0,4),c(1,0,5)), byrow = T, ncol = 3),
+                Green	= matrix(c(c(0,1,1),c(1,1,1),c(2,0,1),c(1,1,2),c(2,0,2),c(2,0,3)), byrow = T, ncol = 3),
+                Yellow	= matrix(c(c(0,1,0),c(1,2,0),c(2,0,1),c(1,1,1),c(0,1,2),c(1,0,2)), byrow = T, ncol = 3)
   )
   
   aux2 <- sample.int(n = nrow(aux), size = amount, replace = T)
@@ -36,16 +39,62 @@ pair_of_dices <- function(dice1, dice2, amount_of_pairs){
   return(output)
   
 }
-out <- pair_of_dices("red", "blue", 10000)
+basic_weapon_information <- function(item){
+  
+  href <- item["href"]
+  title <- item["title"]
+  
+  items_web <- read_html(paste0("https://imperial-assault.fandom.com",href))
+  
+  item_attributes <- items_web %>% html_nodes(".pi-font")
+  item_attributes_text <- item_attributes %>% html_text
+  
+  name <- title
+  tier <- item_attributes_text[1]
+  type <- item_attributes_text[3]
+  upgrades <- item_attributes_text[4]
+  cost <- item_attributes_text[5] 
+  item_dices <- item_attributes %>% html_nodes(xpath = "//*[(@title = 'Dice')]/img") %>% html_attrs() %>% 
+    sapply(function(x) sub(".png","",x["data-image-name"]))
+  
+  if(length(item_dices)==0){item_dices <-  c(NA,NA,NA)}
+  
+  output <- data.frame("name" = name, "tier" = tier, "class" = type, 
+                       "upgrades" = upgrades, "cost" = cost, 
+                       "dice1" = item_dices[1], 
+                       "dice2" = item_dices[2],
+                       "dice3" = item_dices[3],
+                       stringsAsFactors = F)
+  
+  return(output)
+}
+retrieve_list_of_weapons <- function(type){
+  
+  if(type %in% c("Ranged", "Melee")){
+  
+    list_of_weapons_web <- read_html(paste0("https://imperial-assault.fandom.com/wiki/Category:",type))
+    
+    list_of_weapons <- list_of_weapons_web %>% 
+      html_nodes(".category-page__member-link") %>% 
+      html_attrs()
+    
+    output <- t(simplify2array(list_of_weapons))[,c("href","title")]
+    return(output)
+  } else {
+    stop("Input can only be 'Ranged' or 'Melee'")
+  }
+  
+}
 
-library(rvest)
+out <- pair_of_dices("Red", "Red", 10000)
+weapon_types <- c("Ranged", "Melee")
 
-items_web <- read_html("https://imperial-assault.fandom.com/wiki/Item_Card_-_By_Tier/Product")
+list_of_weapons <- sapply(weapon_types,retrieve_list_of_weapons)
+list_of_weapons_data <- lapply(list_of_weapons, 
+                               function(x) apply(x, 1, basic_weapon_information))
 
-items_web %>% html_attr()
+basic_info_all_weapons <- bind_rows(lapply(list_of_weapons_data, function(x) bind_rows(x)),.id = "type")
 
-ups <- items_web %>% 
-  html_node(xpath = "//*[(@id = 'WikiaArticle')]/*[(@id = 'mw-content-text')]/ul") 
 
-ups2 <- ups %>% 
-  html_nodes(xpath = ".//a") %>% html_attrs()
+
+
